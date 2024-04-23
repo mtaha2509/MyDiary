ea3587c6fac29d9007615b9ce1cf5bad3b077fb0
Syed Muhammad Hassaan Ali <alihassaan480@gmail.com>
2024-04-24 01:57:56 +0500

    Modified Footer

--- src/components/LandingPage/footer/Footer.jsx
+++ src/components/LandingPage/footer/Footer.jsx
@@ -4,73 +4,69 @@ import { Box } from "@mui/material";
 import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
-import {Logo2} from "../../../assets";
+import { Logo2 } from "../../../assets";
 
 function Footer() {
-	return (
-		<Box>
-			<footer className="footer">
-				<div className="footer-links">
-					<Container>
-						<Row>
-							<Col lg="3">
-								<img src={Logo2} alt="Footer Logo" />
-							</Col>
-							<Col lg="3">
+    return (
+        <Box>
+            <footer className="footer">
+                <div className="footer-links">
+                    <Container>
+                        <Row>
+                            <Col lg="4">
+                                <img src={Logo2} alt="Footer Logo" />
+                            </Col>
+							<Col lg="4">
 								<div className="footer-column">
 									<h2 style={{ fontSize: "20px", fontWeight: 'bold' }}>Company</h2>
-									<ul style={{ listStyleType: 'none', padding: 0, color: 'black' }}>
+									<ul className="footer-list">
 										<li><a href="#">About Us</a></li>
-										<li style={{ whiteSpace: 'nowrap' }}><a href="#">Partner Program</a></li>
+										<li><a href="#">Partner Program</a></li>
 										<li><a href="#">Career</a></li>
 										<li><a href="#">Contact Us</a></li>
 										<li><a href="#">Privacy Policy</a></li>
 									</ul>
 								</div>
 							</Col>
-							<Col lg="3">
-								<div className="footer-column">
-									<h2 style={{ fontSize: "20px", fontWeight: 'bold', whiteSpace: 'nowrap' }}>Social Media</h2>
-									<a href="#">
-										<i className="fab fa-facebook-f"></i>
-										<span>Facebook</span>
-									</a>
-									<a href="#">
-										<i className="fab fa-instagram"></i>
-										<span>Instagram</span>
-									</a>
-									<a href="#">
-										<i className="fab fa-twitter"></i>
-										<span>Twitter</span>
-									</a>
-									<a href="#">
-										<i className="fab fa-youtube"></i>
-										<span>Youtube</span>
-									</a>
-								</div>
-							</Col>
-							<Col lg="3">
+							<Col lg="4">
 								<div className="footer-column">
 									<h2 style={{ fontSize: "20px", fontWeight: 'bold', whiteSpace: 'nowrap' }}>Contact Us</h2>
-									<a href="#">Lahore</a>
-									<a href="#">Karachi</a>
-									<a href="#">Islamabad</a>
-									<a href="#">Faislabad</a>
+									<ul className="footer-list">
+										<li><a href="#">Lahore</a></li>
+										<li><a href="#">Karachi</a></li>
+										<li><a href="#">Islamabad</a></li>
+										<li><a href="#">Faislabad</a></li>
+									</ul>
 								</div>
 							</Col>
-						</Row>
-						<hr className="footer-line" style={{ marginTop: '40px' }} />
-						<Row>
-							<Col lg="12">
-								<div className="footer-bottom">
-									<p className="text-center" style={{ marginTop: '40px' }}>
-										© 2024 MyDiary+, Inc. All rights reserved
-									</p>
-								</div>
-							</Col>
-						</Row>
-
-					</Container>
+                        </Row>
+                        <hr className="footer-line" style={{ marginTop: '0px' }} />
+                        <Row>
+                            <Col lg="6">
+                                <div className="footer-bottom" style={{ marginTop: '0px' }}>
+                                    <p style={{ marginBottom: '0px', marginLeft: '20px' }}>
+                                        © 2024 MyDiary+, Inc. All rights reserved
+                                    </p>
+                                </div>
+                            </Col>
+                            <Col lg="6">
+                                <div className="d-flex justify-content-end">
+										<a className="FB-Hover" href="#">
+											<img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSXoMFtNYy-gfuvVnQkKSiDAmfYt0ynmaGz55WPNbUPZw&s" alt="Facebook" width="27" height="27" style={{ marginRight: '30px'}} />
+										</a>
+										<a className="IG-Hover" href="#">
+											<img src="https://upload.wikimedia.org/wikipedia/commons/thumb/9/95/Instagram_logo_2022.svg/1200px-Instagram_logo_2022.svg.png" alt="Instagram" width="30" height="30" style={{ marginRight: '30px', borderRadius: '100%' }} />
+										</a>
+										<a className="LI-Hover" href="#">
+											<img src="https://upload.wikimedia.org/wikipedia/commons/thumb/f/f8/LinkedIn_icon_circle.svg/2048px-LinkedIn_icon_circle.svg.png" alt="LinkedIn" width="27" height="27" style={{ marginRight: '30px' }} />
+										</a>
+										<a className="T-Hover" href="">
+											<img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTv96fZKwjUHR8ipNKPnKyaAHzn3MQUAERWelvryUg1rA&s" alt="Twitter" width="27" height="27" style={{ marginRight: '10px', borderRadius: '80%' }} />
+										</a>
+                                </div>
+                            </Col>
+                        </Row>
+                    </Container>
                 </div>
             </footer>
         </Box>
