9ba3930c24bdd8a01adc1fd81af5df44d1f62014
mtaha2509 <ostrich.mt2@gmail.com>
2024-05-14 20:13:45 +0500

    Added Pagination

--- front-end/src/components/DiaryEntryPage/page.jsx
+++ front-end/src/components/DiaryEntryPage/page.jsx
@@ -6,14 +6,62 @@ import { Diary_temp1, TimeCapsule } from "./../../assets"; // Import templates
 function DiaryEntryPage() {
   // Define templates array with IDs starting from 1
   const templates = [
-    { id: 1, name: "Template 1", image: "https://img.freepik.com/free-photo/abstract-uv-ultraviolet-light-composition_23-2149243965.jpg?t=st=1715459506~exp=1715463106~hmac=5e664728824588fd753a23dbf3836488c146aad664f6f2108d1c6e8d828586f7&w=1380", description: "Description for Template 1" },
-    { id: 2, name: "Template 2", image: "https://img.freepik.com/free-photo/blue-smooth-wall-textured-background_53876-106133.jpg?t=st=1715459690~exp=1715463290~hmac=8207a29193ae4dfe44b85d8cc8c1399016bd1cf6b464c0040e2a4eb9c1e99de7&w=1380", description: "Description for Template 2" },
-    { id: 3, name: "Template 3", image: "https://img.freepik.com/free-photo/nature-design-with-bokeh-effect_1048-1882.jpg?t=st=1715464851~exp=1715468451~hmac=2076d9dbb05699da4c06a753588179fde54f14abecd72256ca6c8ccec6d65c36&w=826", description: "Description for Template 3" },
-    { id: 4, name: "Template 4", image: "https://img.freepik.com/free-photo/blue-wall-background_53876-88663.jpg?t=st=1715464876~exp=1715468476~hmac=e60797571a897a4de61ef6b428ec4599590192007dc1ac8194fe2ac97b8970e6&w=740", description: "Description for Template 4" },
-    { id: 5, name: "Template 5", image: "https://img.freepik.com/free-vector/blue-curve-background_53876-113112.jpg?t=st=1715464915~exp=1715468515~hmac=c26c04fb46c8ccd474a8a1b9e33224fb58c303443bc7fe6d93c44ff98f80ef9c&w=1380", description: "Description for Template 5" },
-    { id: 6, name: "Template 6", image: "https://img.freepik.com/free-vector/yellow-comic-zoom-lines-background_1017-15136.jpg?t=st=1715464965~exp=1715468565~hmac=34c4e8c67651b95461462c80385b56fd1a4b4c84d764c07d05acad3ecfe116b0&w=740", description: "Description for Template 6"},
-    { id: 7, name: "Template 7", image: "https://img.freepik.com/free-vector/abstract-organic-pattern-design-background_1048-19286.jpg?t=st=1715465022~exp=1715468622~hmac=aa40c462b208f2ecc0df158e15c9322ea2f740d279cbd0c3f9b5e01c25574207&w=740", description: "Description for Template 7"},
-    { id: 8, name: "Template 8", image: "https://img.freepik.com/free-photo/dark-blue-product-background_53876-92801.jpg?t=st=1715465092~exp=1715468692~hmac=9dadad91de91df1e59a03fb6c6aac4d7c71b5b50d04b640855e20e81665d1dfd&w=1380", description: "Description for Template 8"}
+    {
+      id: 1,
+      name: "Template 1",
+      image:
+        "https://img.freepik.com/free-photo/abstract-uv-ultraviolet-light-composition_23-2149243965.jpg?t=st=1715459506~exp=1715463106~hmac=5e664728824588fd753a23dbf3836488c146aad664f6f2108d1c6e8d828586f7&w=1380",
+      description: "Description for Template 1",
+    },
+    {
+      id: 2,
+      name: "Template 2",
+      image:
+        "https://img.freepik.com/free-photo/blue-smooth-wall-textured-background_53876-106133.jpg?t=st=1715459690~exp=1715463290~hmac=8207a29193ae4dfe44b85d8cc8c1399016bd1cf6b464c0040e2a4eb9c1e99de7&w=1380",
+      description: "Description for Template 2",
+    },
+    {
+      id: 3,
+      name: "Template 3",
+      image:
+        "https://img.freepik.com/free-photo/nature-design-with-bokeh-effect_1048-1882.jpg?t=st=1715464851~exp=1715468451~hmac=2076d9dbb05699da4c06a753588179fde54f14abecd72256ca6c8ccec6d65c36&w=826",
+      description: "Description for Template 3",
+    },
+    {
+      id: 4,
+      name: "Template 4",
+      image:
+        "https://img.freepik.com/free-photo/blue-wall-background_53876-88663.jpg?t=st=1715464876~exp=1715468476~hmac=e60797571a897a4de61ef6b428ec4599590192007dc1ac8194fe2ac97b8970e6&w=740",
+      description: "Description for Template 4",
+    },
+    {
+      id: 5,
+      name: "Template 5",
+      image:
+        "https://img.freepik.com/free-vector/blue-curve-background_53876-113112.jpg?t=st=1715464915~exp=1715468515~hmac=c26c04fb46c8ccd474a8a1b9e33224fb58c303443bc7fe6d93c44ff98f80ef9c&w=1380",
+      description: "Description for Template 5",
+    },
+    {
+      id: 6,
+      name: "Template 6",
+      image:
+        "https://img.freepik.com/free-vector/yellow-comic-zoom-lines-background_1017-15136.jpg?t=st=1715464965~exp=1715468565~hmac=34c4e8c67651b95461462c80385b56fd1a4b4c84d764c07d05acad3ecfe116b0&w=740",
+      description: "Description for Template 6",
+    },
+    {
+      id: 7,
+      name: "Template 7",
+      image:
+        "https://img.freepik.com/free-vector/abstract-organic-pattern-design-background_1048-19286.jpg?t=st=1715465022~exp=1715468622~hmac=aa40c462b208f2ecc0df158e15c9322ea2f740d279cbd0c3f9b5e01c25574207&w=740",
+      description: "Description for Template 7",
+    },
+    {
+      id: 8,
+      name: "Template 8",
+      image:
+        "https://img.freepik.com/free-photo/dark-blue-product-background_53876-92801.jpg?t=st=1715465092~exp=1715468692~hmac=9dadad91de91df1e59a03fb6c6aac4d7c71b5b50d04b640855e20e81665d1dfd&w=1380",
+      description: "Description for Template 8",
+    },
   ];
 
   // State for tracking selected template
@@ -30,14 +78,18 @@ function DiaryEntryPage() {
   const [currentPage, setCurrentPage] = useState(1);
   const [charLimit, setCharLimit] = useState(200); // Character limit per page
   const [remainingChars, setRemainingChars] = useState(charLimit);
-  const [diaryPages, setDiaryPages] = useState([{ pageNumber: 1, title: "", content: "" }]);
+
+  // State for diary entries
+  const [diaryEntries, setDiaryEntries] = useState([
+    { pageNumber: 1, title: "", content: "" },
+  ]);
 
   useEffect(() => {
     // Calculate remaining characters
     const contentLength = diaryContent.length;
-    const remaining = charLimit - (currentPage - 1) * charLimit - contentLength;
+    const remaining = charLimit - contentLength;
     setRemainingChars(remaining);
-  }, [currentPage, diaryContent, charLimit]);
+  }, [diaryContent, charLimit]);
 
   const toggleSidebar = () => {
     setSidebarExpanded(!sidebarExpanded);
@@ -71,37 +123,59 @@ function DiaryEntryPage() {
   const handleDiaryContentChange = (e) => {
     const content = e.target.value;
     const contentLength = content.length;
-    
-    // Calculate the remaining characters on the current page
-    const remainingOnCurrentPage = charLimit - (contentLength % charLimit);
-  
-    if (contentLength <= charLimit * currentPage) {
-      setDiaryContent(content);
-      setRemainingChars(remainingOnCurrentPage);
-    } else {
-      // Save content of the current page before moving to the next
-      const updatedPages = diaryPages.map(page => {
-        if (page.pageNumber === currentPage) {
-          return { ...page, content: content.slice(0, charLimit) };
+
+    if (contentLength <= charLimit) {
+      // Update content of the current page
+      const updatedEntries = diaryEntries.map((entry) => {
+        if (entry.pageNumber === currentPage) {
+          return { ...entry, content };
         }
-        return page;
+        return entry;
       });
-      setDiaryPages(updatedPages);
+      setDiaryEntries(updatedEntries);
+      setDiaryContent(content);
+      setRemainingChars(charLimit - contentLength);
+    } else {
+      // Save content of the current page
+      const updatedEntries = diaryEntries.map((entry) => {
+        if (entry.pageNumber === currentPage) {
+          return { ...entry, content: content.slice(0, charLimit) };
+        }
+        return entry;
+      });
+      setDiaryEntries(updatedEntries);
 
       // Move to the next page
       setCurrentPage(currentPage + 1);
-      const remainingOnNextPage = charLimit - (contentLength % charLimit);
-      setDiaryContent(content.slice(0, contentLength - remainingOnNextPage));
-      setRemainingChars(remainingOnNextPage);
+      setDiaryContent(content.slice(charLimit));
+      setRemainingChars(charLimit - content.slice(charLimit).length);
+
+      // Add a new page to the diary entries
+      setDiaryEntries([
+        ...updatedEntries,
+        {
+          pageNumber: currentPage + 1,
+          title: diaryTitle,
+          content: content.slice(charLimit),
+        },
+      ]);
     }
   };
 
   const handleNextPage = () => {
-    setCurrentPage(Math.min(currentPage + 1, totalPages));
+    if (currentPage < diaryEntries.length) {
+      setCurrentPage(currentPage + 1);
+      setDiaryContent(diaryEntries[currentPage].content);
+      setDiaryTitle(diaryEntries[currentPage].title);
+    }
   };
 
   const handlePreviousPage = () => {
-    setCurrentPage(Math.max(currentPage - 1, 1));
+    if (currentPage > 1) {
+      setCurrentPage(currentPage - 1);
+      setDiaryContent(diaryEntries[currentPage - 2].content);
+      setDiaryTitle(diaryEntries[currentPage - 2].title);
+    }
   };
 
   const handleBackButtonClick = () => {
@@ -111,7 +185,10 @@ function DiaryEntryPage() {
   return (
     <>
       <div className="page-container">
-        <SideBar toggleSidebar={toggleSidebar} sidebarExpanded={sidebarExpanded} />
+        <SideBar
+          toggleSidebar={toggleSidebar}
+          sidebarExpanded={sidebarExpanded}
+        />
         <div className="page-content">
           <MainNavBar />
 
@@ -120,7 +197,9 @@ function DiaryEntryPage() {
             <div
               className="template-slot"
               style={{
-                backgroundImage: selectedTemplate ? `url(${selectedTemplate.image})` : "none",
+                backgroundImage: selectedTemplate
+                  ? `url(${selectedTemplate.image})`
+                  : "none",
                 backgroundSize: "cover",
                 backgroundRepeat: "no-repeat",
                 backgroundPosition: "center",
@@ -135,7 +214,17 @@ function DiaryEntryPage() {
                     className="diary-title"
                     placeholder="Title"
                     value={diaryTitle}
-                    onChange={(e) => setDiaryTitle(e.target.value)}
+                    onChange={(e) => {
+                      const title = e.target.value;
+                      setDiaryTitle(title);
+                      const updatedEntries = diaryEntries.map((entry) => {
+                        if (entry.pageNumber === currentPage) {
+                          return { ...entry, title };
+                        }
+                        return entry;
+                      });
+                      setDiaryEntries(updatedEntries);
+                    }}
                     style={{
                       position: "absolute",
                       top: "10%",
@@ -145,7 +234,7 @@ function DiaryEntryPage() {
                       border: "none",
                       fontSize: "2rem",
                       fontWeight: "bold",
-                      color: "white"
+                      color: "white",
                     }}
                   />
                   <textarea
@@ -164,23 +253,46 @@ function DiaryEntryPage() {
                       border: "none",
                       fontSize: "1.5rem",
                       color: "white",
-                      resize: "none"
+                      resize: "none",
                     }}
                   />
-                  <p style={{ position: "absolute", bottom: "10%", left: "50%", transform: "translate(-50%, -50%)" }}>
-                    {remainingChars >= 0 ? `Remaining characters: ${remainingChars}` : ""}
+                  <p
+                    style={{
+                      position: "absolute",
+                      bottom: "10%",
+                      left: "50%",
+                      transform: "translate(-50%, -50%)",
+                    }}
+                  >
+                    {remainingChars >= 0
+                      ? `Remaining characters: ${remainingChars}`
+                      : ""}
                   </p>
                   <div className="pagination-buttons">
-                    <button onClick={handlePreviousPage} disabled={currentPage === 1}>
+                    <button
+                      onClick={handlePreviousPage}
+                      disabled={currentPage === 1}
+                    >
                       &lt; Prev
                     </button>
                     <span>Page {currentPage}</span>
-                    <button onClick={handleNextPage} disabled={remainingChars <= 0}>
+                    <button
+                      onClick={handleNextPage}
+                      disabled={currentPage >= diaryEntries.length}
+                    >
                       Next &gt;
                     </button>
                   </div>
                   {currentPage > 1 && (
-                    <button onClick={handleBackButtonClick} style={{ position: "absolute", top: "90%", left: "50%", transform: "translate(-50%, -50%)" }}>
+                    <button
+                      onClick={handleBackButtonClick}
+                      style={{
+                        position: "absolute",
+                        top: "90%",
+                        left: "50%",
+                        transform: "translate(-50%, -50%)",
+                      }}
+                    >
                       Go Back
                     </button>
                   )}
@@ -190,7 +302,10 @@ function DiaryEntryPage() {
                 <div className="empty-slot">
                   <p>First you need to select a Template</p>
                   <div>
-                    <button onClick={openModal} className="choose-template-button">
+                    <button
+                      onClick={openModal}
+                      className="choose-template-button"
+                    >
                       Choose A template
                     </button>
                   </div>
@@ -199,7 +314,9 @@ function DiaryEntryPage() {
             </div>
             {selectedTemplate && ( // Render the replace button only if a template is selected
               <div className="replace-template-button">
-                <button onClick={handleReplaceTemplate}>Replace Template</button>
+                <button onClick={handleReplaceTemplate}>
+                  Replace Template
+                </button>
               </div>
             )}
           </div>
@@ -216,7 +333,9 @@ function DiaryEntryPage() {
                   <img src={template.image} alt={template.name} />
                   <h3>{template.name}</h3>
                   <p>{template.description}</p>
-                  <button onClick={() => handleTemplateSelect(template)}>Select</button>
+                  <button onClick={() => handleTemplateSelect(template)}>
+                    Select
+                  </button>
                 </div>
               ))}
             </div>
--- front-end/src/components/TimeCapsule/TimeCapsule.jsx
+++ front-end/src/components/TimeCapsule/TimeCapsule.jsx
@@ -1,28 +1,35 @@
-import React, { useState } from 'react';
-import './TimeCapsule.css';
-import Sidebar from '../DiaryEntryPage/SideBar/Sidebar';
-import timeCapsuleImage from '../../assets/TimeCapsule.svg';
+import React, { useState } from "react";
+import "./TimeCapsule.css";
+import Sidebar from "../DiaryEntryPage/SideBar/Sidebar";
+import timeCapsuleImage from "../../assets/TimeCapsule.svg";
 
 const TimeCapsule = () => {
+  const [sidebarExpanded, setSidebarExpanded] = useState(false);
   const [entries, setEntries] = useState([]);
-  const [newEntry, setNewEntry] = useState({ interval: '', messageToFutureSelf: '', uploadedImage: null });
+  const [newEntry, setNewEntry] = useState({
+    interval: "",
+    messageToFutureSelf: "",
+    uploadedImage: null,
+  });
   const [dragging, setDragging] = useState(false);
   const [currentPage, setCurrentPage] = useState(1);
   const entriesPerPage = 3;
 
   const handleInputChange = (e) => {
     const { name, value } = e.target;
-    setNewEntry(prevState => ({
+    setNewEntry((prevState) => ({
       ...prevState,
-      [name]: value
+      [name]: value,
     }));
   };
-
+  const toggleSidebar = () => {
+    setSidebarExpanded(!sidebarExpanded);
+  };
   const handleImageChange = (e) => {
     const file = e.target.files[0];
-    setNewEntry(prevState => ({
+    setNewEntry((prevState) => ({
       ...prevState,
-      uploadedImage: file
+      uploadedImage: file,
     }));
   };
 
@@ -41,34 +48,33 @@ const TimeCapsule = () => {
   };
 
   const handleDrop = (e) => {
-    e.preventDefault()
+    e.preventDefault();
     setDragging(false);
-    
+
     const file = e.dataTransfer.files[0];
-    
+
     if (file) {
       const reader = new FileReader();
       reader.onload = () => {
-        setNewEntry(prevState => ({
+        setNewEntry((prevState) => ({
           ...prevState,
-          uploadedImage: reader.result
+          uploadedImage: reader.result,
         }));
       };
       reader.readAsDataURL(file);
     }
   };
-  
 
   const handleSubmit = (e) => {
     e.preventDefault();
     const currentDate = new Date().toLocaleDateString();
-    setEntries(prevEntries => [
+    setEntries((prevEntries) => [
       ...prevEntries,
-      { ...newEntry, date: currentDate }
+      { ...newEntry, date: currentDate },
     ]);
-    setNewEntry({ interval: '', messageToFutureSelf: '', uploadedImage: null });
+    setNewEntry({ interval: "", messageToFutureSelf: "", uploadedImage: null });
   };
-  
+
   const totalPages = Math.ceil(entries.length / entriesPerPage);
   const indexOfLastEntry = currentPage * entriesPerPage;
   const indexOfFirstEntry = indexOfLastEntry - entriesPerPage;
@@ -76,26 +82,55 @@ const TimeCapsule = () => {
   const paginate = (pageNumber) => setCurrentPage(pageNumber);
 
   return (
-    <div className='fadein'>
+    <div className="fadein">
       <div className="container-fluid">
         <div className="col-lg-12 offset-lg-12">
           <div className="parent-container">
-            <img src="https://www.michigandaily.com/wp-content/uploads/2023/05/Untitled_Artwork-84.png" alt="Time Capsule" style={{ height:'500px', marginTop: '0px', width: '48%' }} />
+            <img
+              src="https://www.michigandaily.com/wp-content/uploads/2023/05/Untitled_Artwork-84.png"
+              alt="Time Capsule"
+              style={{ height: "500px", marginTop: "0px", width: "48%" }}
+            />
           </div>
           <div className="time-capsule-header text-center">
-            <h1 className="page-title" style={{ fontWeight: 'bold'}}>Welcome to Time Capsule</h1>
-            <p className="page-subtitle" style={{ fontWeight: 'bold' }}>Plant your Time Capsule and let it revive your beloved memories</p>
+            <h1 className="page-title" style={{ fontWeight: "bold" }}>
+              Welcome to Time Capsule
+            </h1>
+            <p className="page-subtitle" style={{ fontWeight: "bold" }}>
+              Plant your Time Capsule and let it revive your beloved memories
+            </p>
           </div>
         </div>
       </div>
       <div className="col-lg-6 offset-lg-3">
         <div className="p-2">
-          <Sidebar currentPage="TimeCapsule" />
-          <div className="time-capsule-header" style={{ marginLeft:'200px', borderRadius:'80px' }} >
-            <h2 className="text-center" style={{ fontWeight: 'bold' }}>Plant a Time Capsule</h2>
+          <Sidebar
+            toggleSidebar={toggleSidebar}
+            sidebarExpanded={sidebarExpanded}
+          />
+          <div
+            className="time-capsule-header"
+            style={{ marginLeft: "200px", borderRadius: "80px" }}
+          >
+            <h2 className="text-center" style={{ fontWeight: "bold" }}>
+              Plant a Time Capsule
+            </h2>
           </div>
-          <div className="card-body" style={{ backgroundColor: '#c1c1c1', padding:'20px', borderRadius:'20px' }}>
-            <form onSubmit={handleSubmit} onDragOver={handleDragOver} onDragEnter={handleDragEnter} onDragLeave={handleDragLeave} onDrop={handleDrop}>
+          <div
+            className="card-body"
+            style={{
+              backgroundColor: "#c1c1c1",
+              padding: "20px",
+              borderRadius: "20px",
+            }}
+          >
+            <form
+              onSubmit={handleSubmit}
+              onDragOver={handleDragOver}
+              onDragEnter={handleDragEnter}
+              onDragLeave={handleDragLeave}
+              onDrop={handleDrop}
+            >
               <select
                 name="interval"
                 value={newEntry.interval}
@@ -120,9 +155,13 @@ const TimeCapsule = () => {
                 />
               </div>
               <div className="form-group mt-3">
-                <input type="file" accept="image/*" onChange={handleImageChange} />
+                <input
+                  type="file"
+                  accept="image/*"
+                  onChange={handleImageChange}
+                />
                 <div
-                  className={`drag-drop-area ${dragging ? 'active' : ''}`}
+                  className={`drag-drop-area ${dragging ? "active" : ""}`}
                   onDragOver={handleDragOver}
                   onDragEnter={handleDragEnter}
                   onDragLeave={handleDragLeave}
@@ -131,24 +170,42 @@ const TimeCapsule = () => {
                   Drag & Drop or Click to Upload
                 </div>
               </div>
-              <div className="form-group mt-3" style={{ textAlign: 'center' }}>
-                <button type="submit" className="btn btn-primary">Set Time Capsule</button>
+              <div className="form-group mt-3" style={{ textAlign: "center" }}>
+                <button type="submit" className="btn btn-primary">
+                  Set Time Capsule
+                </button>
               </div>
             </form>
           </div>
         </div>
       </div>
       <div className="col-lg-6 offset-lg-3">
-        <h2 className="text-center" style={{ fontWeight: 'bold' }}>Diary Entries</h2>
-        <div className="diary-entries" style={{ backgroundColor: '#c1c1c1', padding:'30px', borderRadius:'20px'}}>
+        <h2 className="text-center" style={{ fontWeight: "bold" }}>
+          Diary Entries
+        </h2>
+        <div
+          className="diary-entries"
+          style={{
+            backgroundColor: "#c1c1c1",
+            padding: "30px",
+            borderRadius: "20px",
+          }}
+        >
           {currentEntries.map((entry, index) => (
             <div key={index} className="card mb-3">
               <div className="card-body">
-                <h5 className="card-title" style={{ fontWeight: 'bold' }}>Capsule {index + indexOfFirstEntry + 1}</h5>
+                <h5 className="card-title" style={{ fontWeight: "bold" }}>
+                  Capsule {index + indexOfFirstEntry + 1}
+                </h5>
                 <p className="card-text">Capsule Planted Date: {entry.date}</p>
                 <p className="card-text">Interval: {entry.interval}</p>
-                <p className="card-text">Next Opening Date: {getNextOpeningDate(entry.date, entry.interval)}</p>
-                <p className="card-text">Message: {entry.messageToFutureSelf}</p>
+                <p className="card-text">
+                  Next Opening Date:{" "}
+                  {getNextOpeningDate(entry.date, entry.interval)}
+                </p>
+                <p className="card-text">
+                  Message: {entry.messageToFutureSelf}
+                </p>
               </div>
             </div>
           ))}
@@ -157,9 +214,19 @@ const TimeCapsule = () => {
         {totalPages > 1 && (
           <nav aria-label="Page navigation">
             <ul className="pagination justify-content-center">
-              {[...Array(totalPages).keys()].map(page => (
-                <li key={page} className={`page-item ${page + 1 === currentPage ? 'active' : ''}`}>
-                  <button onClick={() => paginate(page + 1)} className="page-link">{page + 1}</button>
+              {[...Array(totalPages).keys()].map((page) => (
+                <li
+                  key={page}
+                  className={`page-item ${
+                    page + 1 === currentPage ? "active" : ""
+                  }`}
+                >
+                  <button
+                    onClick={() => paginate(page + 1)}
+                    className="page-link"
+                  >
+                    {page + 1}
+                  </button>
                 </li>
               ))}
             </ul>
@@ -173,16 +240,16 @@ const TimeCapsule = () => {
 const getNextOpeningDate = (plantedDate, interval) => {
   const date = new Date(plantedDate);
   switch (interval) {
-    case 'Daily':
+    case "Daily":
       date.setDate(date.getDate() + 1);
       break;
-    case 'Weekly':
+    case "Weekly":
       date.setDate(date.getDate() + 7);
       break;
-    case 'Monthly':
+    case "Monthly":
       date.setMonth(date.getMonth() + 1);
       break;
-    case 'Yearly':
+    case "Yearly":
       date.setFullYear(date.getFullYear() + 1);
       break;
     default:
