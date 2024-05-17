ae6ad09bea3278aa3fc2ef4e7eef2427ae37cfaa
Syed Muhammad Hassaan Ali <alihassaan480@gmail.com>
2024-05-17 20:48:25 +0500

    Fixed Landing Page

--- front-end/src/components/LandingPage/Section1/Section1.jsx
+++ front-end/src/components/LandingPage/Section1/Section1.jsx
@@ -4,35 +4,31 @@ import "./Section1.css";
 import list from "./Component";
 
 function Section1() {
-  function CreateEntry(Item) {
-    return (
-      <div
-        className="col-lg-4 col-md-6 col-sm-12 entry-column"
-        key={Item.title}
-      >
-        <Entry
-          color={Item.color}
-          img={Item.SVG}
-          title={Item.title}
-          desc={Item.desc}
-        />
-      </div>
-    );
-  }
-
   return (
-    <section className="section1">
-      <div className="section1-container container">
+    <div className="section">
         <div className="section1-content">
           <h2>
             Unlock your journaling journey
             <br />
             <span>Reflect, Discover, You.</span>
           </h2>
-          <div className="row">{list.map(CreateEntry)}</div>
+          <div className="row">
+            {list.map((Item) => (
+              <div
+                className="col-lg-4 col-md-6 col-sm-12 entry-column"
+                key={Item.title}
+              >
+                <Entry
+                  color={Item.color}
+                  img={Item.SVG}
+                  title={Item.title}
+                  desc={Item.desc}
+                />
+              </div>
+            ))}
+          </div>
         </div>
-      </div>
-    </section>
+    </div>
   );
 }
 
