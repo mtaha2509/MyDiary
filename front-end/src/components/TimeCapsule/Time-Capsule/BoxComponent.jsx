3000a2bdd8c490d1205e39656819e36661f22bd3
mtaha2509 <ostrich.mt2@gmail.com>
2024-05-18 10:57:12 +0500

    Revert to commit afabe1d

--- front-end/src/components/TimeCapsule/Time-Capsule/BoxComponent.jsx
+++ front-end/src/components/TimeCapsule/Time-Capsule/BoxComponent.jsx
@@ -1,5 +1,5 @@
-import React from 'react';
-import './BoxComponent.css'; // Import your CSS file
+import React from "react";
+import "./BoxComponent.css"; // Import your CSS file
 
 const BoxComponent = () => {
   return (
@@ -9,8 +9,16 @@ const BoxComponent = () => {
           <div className="box">
             <div className="box-body">
               <div className="items-container">
-                <img className="item shoe" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTQ1VVztH2GrKmWF1tIyF-00gnfu89byVhwMxLbV-d9jQ&amp;s" alt="shoe" />
-                <img className="item watch" src="https://ronin.pk/cdn/shop/files/R-09SmartWatchSilver1.png?v=1708364244" alt="watch" />
+                <img
+                  className="item shoe"
+                  src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTQ1VVztH2GrKmWF1tIyF-00gnfu89byVhwMxLbV-d9jQ&amp;s"
+                  alt="shoe"
+                />
+                <img
+                  className="item watch"
+                  src="https://ronin.pk/cdn/shop/files/R-09SmartWatchSilver1.png?v=1708364244"
+                  alt="watch"
+                />
               </div>
               <div className="box-lid"></div>
             </div>
